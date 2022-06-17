4283dbef84de4d976475ce00aa660276393fbefd
anne ju <anneju.824@gmail.com>
2022-06-17 23:33:25 +0800

    feat/return-user-clock_ins: add index spec

--- spec/requests/clock_ins_spec.rb
+++ spec/requests/clock_ins_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "ClockIns", type: :request do
+RSpec.describe 'ClockIns', type: :request do
   let(:user) { create(:user) }
 
-  describe "GET /index" do
+  describe 'GET /index' do
     let!(:clock_in_1) { create(:clock_in, user: user, created_at: Time.current) }
     let!(:clock_in_2) { create(:clock_in, user: user, created_at: Time.current + 10) }
     let!(:clock_in_3) { create(:clock_in, user: user, created_at: Time.current + 20) }
 
-    it "returns 200 status and clock_ins records" do
-      get "/users/#{user.id}/clock_ins"
+    before { get "/users/#{user.id}/clock_ins" }
 
+    it 'returns 200 status and clock_ins records' do
       expect(response.status).to eq(200)
+      expect(json_response['clock_ins'].size).to eq(3)
+    end
+
+    it 'return clock_ins records by created_at desc order' do
+      expect(json_response['clock_ins'].pluck('id')).to eq([clock_in_3.id, clock_in_2.id, clock_in_1.id])
     end
   end
 end
