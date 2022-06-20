9e8094c8cdeb6ebd0bd88cb8a966f68ea4c8e60a
anne ju <anneju.824@gmail.com>
2022-06-20 22:38:56 +0800

    feat/retrieve-sleep_records: gather in shared context

--- spec/support/users/shared_contexts.rb
+++ spec/support/users/shared_contexts.rb
@@ -9,3 +9,13 @@ shared_context 'when_current_user_is_following_other_users' do
   end
 end
 
+shared_context 'when_current_user_is_followed_by_other_users' do
+  let(:current_user) { create(:user) }
+  let(:other_user_1) { create(:user) }
+  let(:other_user_2) { create(:user) }
+
+  before do
+    create(:relationship, follower: other_user_1, followee: current_user)
+    create(:relationship, follower: other_user_2, followee: current_user)
+  end
+end
